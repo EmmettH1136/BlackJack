ede13e4ab806d93f9acaaf0b2710930e93d1a2c8
Emmett Hasley <Ehasley1136@jh-mba158-06.local>
2019-01-16 11:12:32 -0600

    Initial Commit

---
+++ BlackJack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BlackJack
+//
+//  Created by Emmett Hasley on 1/16/19.
+//  Copyright © 2019 John Heresy High School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
